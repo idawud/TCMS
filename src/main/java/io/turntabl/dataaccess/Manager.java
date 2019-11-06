9cffc98263cc9551c4043a0cfd4c90aca4d9ce3a
erbynn <john.erbynn@gmail.com>
2019-11-06 15:08:03 +0000

    finding name matches ...

--- src/main/java/io/turntabl/dataaccess/Manager.java
+++ src/main/java/io/turntabl/dataaccess/Manager.java
@@ -1,5 +1,6 @@
 package io.turntabl.dataaccess;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,11 +11,25 @@ public class Manager {
         this.clients = clients;
     }
 
-    public List<String> getClientByName(){
+    public List<String> getClientDetailInfo(){
         List<String> allClientInfo = clients.stream().map(c -> c.getName() + " " + c.getAddress() + " " + c.getTelNumber() + " " + c.getEmail()).collect(Collectors.toList());
         return  allClientInfo;
     }
 
+
+    public List<String> getClientDetailsByNameSearch() {
+        return null;
+    }
+
+    public List<Client> findAllMarches(String nameToSearch){
+        List<Client> matchedClient = new ArrayList<>();
+        for (Client client :
+                clients) {
+            if (client.getName() == nameToSearch) {
+                matchedClient.add(client);
+            }
+        }
+    }
 
 
 }
