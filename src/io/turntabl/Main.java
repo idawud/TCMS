4e602dc97ea96e5a7497cd5d10d7bbccada33967
erbynn <john.erbynn@gmail.com>
2019-11-06 12:07:32 +0000

    clients list created

---
+++ src/io/turntabl/Main.java
@@ -0,0 +1,8 @@
+package io.turntabl;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/main/java/io/turntabl/dataaccess/ClientInformationRetrieval.java
@@ -0,0 +1,21 @@
+package io.turntabl.dataaccess;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ClientInformationRetrieval {
+    List<Client> clients = Arrays.asList(
+            new Client("john", "2334", "553472114", "john.erbynn@gmail.com"),
+            new Client("john", "2334", "553472114", "john.erbynn@gmail.com"),
+            new Client("john", "2334", "553472114", "john.erbynn@gmail.com"),
+            new Client("john", "2334", "553472114", "john.erbynn@gmail.com"),
+            new Client("john", "2334", "553472114", "john.erbynn@gmail.com"),
+            new Client("john", "2334", "553472114", "john.erbynn@gmail.com"),
+            new Client("john", "2334", "553472114", "john.erbynn@gmail.com"),
+            new Client("john", "2334", "553472114", "john.erbynn@gmail.com"),
+            new Client("john", "2334", "553472114", "john.erbynn@gmail.com"),
+            new Client("john", "2334", "553472114", "john.erbynn@gmail.com")
+    );
+
+}
