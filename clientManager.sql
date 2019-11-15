ad6d3220eaa92984e2a6fdb294bc709b43cbca01
patriciakasapa <patriciakasapa07@gmail.com>
2019-11-15 16:10:28 +0000

    Database update

--- clientManager.sql
+++ clientManager.sql
@@ -7,16 +7,16 @@ create table clients(
 	active boolean default 'true' 
 );
 
-insert into clients (client_name, address, email, tel_num) values ("Mary", "jamaica down", "pat2@gmail", "020 40 234 2324");
-insert into clients (client_name, address, email, tel_num) values ("Alex", "taifa", "rfgth2@gmail", "230 40 234 2324");
-insert into clients (client_name, address, email, tel_num) values ("Dawud", "tantra", "alex13232@gmail", "020 345 77 2324");
-insert into clients (client_name, address, email, tel_num) values ("Erbynn", "burkina", "tttt@gmail", "0334 5666 456");
-insert into clients (client_name, address, email, tel_num) values ("Drake", "madina", "dawudestate@gmail", "0543 40 234 2324");
-insert into clients (client_name, address, email, tel_num) values ("Cindy", "ferary", "tree@gmail", "0330 40 234");
-insert into clients (client_name, address, email, tel_num) values ("Shaggy", "trigga", "rof@gmail", "020 40 234 2324");
-insert into clients (client_name, address, email, tel_num) values ("Kassy", "foreign land", "karabian@gmail", "020 40 234 2324");
-insert into clients (client_name, address, email, tel_num) values ("Doreen", "astra", "drake3@gmail", "05543 234 2324");
-insert into clients (client_name, address, email, tel_num) values ("Funny", "estate", "fresd@gmail", "033 7687 56656");
+insert into clients (client_name, address, email, tel_num) values ('Mary', 'jamaica down', 'pat2@gmail', '020 40 234 2324');
+insert into clients (client_name, address, email, tel_num) values ('Alex', 'taifa', 'rfgth2@gmail', '230 40 234 2324');
+insert into clients (client_name, address, email, tel_num) values ('Dawud', 'tantra', 'alex13232@gmail', '020 345 77 2324');
+insert into clients (client_name, address, email, tel_num) values ('Erbynn', 'burkina', 'tttt@gmail', '0334 5666 456');
+insert into clients (client_name, address, email, tel_num) values ('Drake', 'madina', 'dawudestate@gmail', '0543 40 234 2324');
+insert into clients (client_name, address, email, tel_num) values ('Cindy', 'ferary', 'tree@gmail', '0330 40 234');
+insert into clients (client_name, address, email, tel_num) values ('Shaggy', 'trigga', 'rof@gmail', '020 40 234 2324');
+insert into clients (client_name, address, email, tel_num) values ('Kassy', 'foreign land', 'karabian@gmail', '020 40 234 2324');
+insert into clients (client_name, address, email, tel_num) values ('Doreen', 'astra', 'drake3@gmail', '05543 234 2324');
+insert into clients (client_name, address, email, tel_num) values ('Funny', 'estate', 'fresd@gmail', '033 7687 56656');
 
 
 
