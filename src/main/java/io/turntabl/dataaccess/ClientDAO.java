8414296ed937ea29917ca13e4cf6318a85ff04b6
idawud <ismaildawud96@gmail.com>
2019-11-15 10:44:48 +0000

    client search

--- src/main/java/io/turntabl/dataaccess/ClientDAO.java
+++ src/main/java/io/turntabl/dataaccess/ClientDAO.java
@@ -7,7 +7,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ClientDAO {
     private Connection connection;
@@ -16,14 +19,22 @@ public class ClientDAO {
         this.connection = DBConnection.connect();
     }
 
-    private ResultSet getResultSet(String queryString) throws SQLException {
+    private ResultSet getResultSet(String queryString, List<String> setValues) throws SQLException {
         PreparedStatement statement = this.connection.prepareStatement(queryString);
+        AtomicInteger count = new AtomicInteger(1);
+        statement.clearParameters();
+        for (String value : setValues) {
+            try {
+                statement.setString(count.get(), value);
+                count.getAndIncrement();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
         return statement.executeQuery();
     }
 
-    public List<Client> getAllClients() throws SQLException {
-        String queryString = "Select * from clients";
-        ResultSet rs = getResultSet(queryString);
+    private List<Client> getClients(ResultSet rs) throws SQLException {
         List<Client> result = new ArrayList<>();
         while ( rs.next()){
             result.add(rowMapper(rs, rs.getRow()));
@@ -31,8 +42,17 @@ public class ClientDAO {
         return result;
     }
 
-    public List<Client> getAllSearchedClients(String clientName){
-        return new ArrayList<>();
+    public List<Client> getAllClients() throws SQLException {
+        String queryString = "SELECT * FROM clients";
+        ResultSet rs = getResultSet(queryString, Collections.emptyList());
+        return getClients(rs);
+    }
+
+
+    public List<Client> getAllSearchedClients(String clientName) throws SQLException {
+        String queryString = "SELECT * FROM clients WHERE client_name = ?";
+        ResultSet rs = getResultSet(queryString, Arrays.asList(clientName));
+        return getClients(rs);
     }
 
     public boolean deleteClient(int id){
